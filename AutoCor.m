549aba8b5d1366f38ccc256a5d9b40552893b31d
Anthony-Ceres <Anthony-CeresR@ripon.edu>
2019-05-05 02:12:04 -0500

    Fixed freqency and several last issues. Likely last version.

--- AutoCor.m
+++ AutoCor.m
@@ -1,76 +1,93 @@
+
+
 clear;
 
 format long
 
 % -- Laser Properties --
 c = 3.*10.^8; % Speed of light in m/s
-wavelength = 800.*10.^-9;
+wavelength = 400.*10.^-9;
 k = 2.*pi./wavelength; % Wave vector
 freq = c./wavelength; % Frequency
+omega = 2*pi*freq; % ANGULAR frequency
 T = 1./freq; % Period
-sigma = .0001; % FWHM of pulse - altar to match the data and report the final value
-%twave1 = .005; % Time offset of wave one to center it
-twave1 = .0005;
-twave2 = .001;
+sigma = 54*10^-15; % FWHM of pulse 107*10^-25 for 800nm at 58
 
-Emax = 0.1; % Arbitrary
 
 % -- Time Settings --
-steps = 30000; % Total steps
-%tf = .00000000000005; % Final time
-tf = .0015; % Final time
-h = tf./steps; % Step size
-tv = zeros(1, steps); % Initialize vector
+numsteps = 12*10^4; % Total steps
+tf = (10)*sigma; %250*10^-15; % Final time (in seconds)
+h = tf./numsteps; % Step size
+tvec = zeros(1, numsteps); % Initialize vector
+Emax = 1; % Arbitrary
+twave1 = 0.5*tf;%initial time of pulse 1 - makes graph look nice
+twave2 = 2.*twave1;
 
-for i=2:steps % Step through and set time vector
-    tv(i) = tv(i-1)+h;
+for i=2:numsteps % Step through and set time vector
+    tvec(i) = tvec(i-1)+h;
 end
 
-% -- Tau Settings
-tausteps = 3000;
-tauf = .001;
-htau = tauf./tausteps;
-tauvec = zeros(1, tausteps);
+%-- Tau Settings --
+numstepsTau = 12*10^3; %total number of steps
+tfTau = tf; %final value of tau
+hTau = tfTau./numstepsTau; %step size of tau
+tauvec = zeros(1,numstepsTau); %initialize vector
 
-Itot = zeros(1,tausteps);
+Itot = zeros(1,numstepsTau);
 
-for i=2:tausteps % Step through and set tau vector
-    tauvec(i) = tauvec(i-1)+htau;
+for i=2:numstepsTau % Step through and set tau vector
+    tauvec(i) = tauvec(i-1)+hTau;
 end
 
-for i=2:tausteps
-    Wave1 = E1(Emax,freq,tv,twave1,sigma);
-    Wave2 = E2(Emax,freq,tv,tauvec(i),twave2,sigma);
-    E = Wave1 + Wave2;
+
+for i=2:numstepsTau
+    Wave1 = E1(Emax,omega,tvec,twave1,sigma);
+    Wave2 = E2(Emax,omega,tvec,tauvec(i),twave2,sigma);
+    E = Wave1+Wave2;
     I = Intensity(E);
-    Itot(i) = trapz(tv,(I.^2));
-    
-    %scatter(phase(1,2:end), I(1,2:end))
-    %scatter(tv(1,2:end), I(1,2:end))
-    %scatter(tv(1,2:end), E(1,2:end))
-
-    % function x = timetopos(t) % Simulates the piezo micrometer, varies 1 micron per 1/2 second
-    % x = 0.0000000000001*t;
-    % end
+    Itot(i)= trapz(tvec,(I.^2)); %numerical integrate function
+    %scatter(tvec(1,2:end), I(1,2:end))
 end
-scatter(tauvec,Itot,1);
 
+plot((tauvec*c),Itot)
+
+peaks = [];
+fwhm = max(Itot)./2;
+for i = 3:length(Itot)
+    if Itot(i) > Itot(i-2) && Itot(i-1) > Itot(i)
+        peaks = [peaks;Itot(i-1)];
+    end
+end
+rangefwhm = peaks(end)-peaks(1)
+numpeak = 0;
+for i = 1:length(peaks)
+    if peaks(i) > fwhm
+        numpeak = numpeak+1;
+    end
+end
+numpeak
 function I = Intensity(E)
-I = 4*(E).^2;
+I = 8*(E).^2;
 end
 
-function E = E2(Emax,freq,tv,tau,twave2,sigma)
-steps = length(tv);
-E = zeros(1,steps);
-for i=1:steps
-    E(i) = Emax*cos(freq*(tv(i)-tau-twave2))*exp((-(tv(i)+tau-twave2).^2)./(2*sigma^2));
+function E = E2(Emax,omega,tvec,tau,twave2,sigma)
+numsteps = length(tvec);
+E = zeros(1,numsteps);
+for i=1:numsteps
+    %E(i) = Emax*cos(k*(0)-freq*tvec(i)+ phase(i));
+    E(i) = Emax*cos(omega*(tvec(i)-tau-twave2))*exp((-(tvec(i)+tau-twave2).^2)./(2*(sigma^2)));
 end
 end
 
-function E = E1(Emax,freq,tv,twave1,sigma)
-steps = length(tv);
-E = zeros(1,steps);
-for i=1:steps
-    E(i) = Emax*cos(freq*(tv(i)-twave1))*exp(-((tv(i)-twave1).^2)./(2*sigma^2));
+function E = E1(Emax,freq,tvec,twave1,sigma)
+numsteps = length(tvec);
+E = zeros(1,numsteps);
+for i=1:numsteps
+    %E(i) = Emax*cos(k*(0)-freq*tvec(i));
+    E(i) = Emax*cos(freq*(tvec(i)-twave1))*exp(-((tvec(i)-twave1).^2)./(2*(sigma^2)));
 end
 end
+
+
+
+
--- AutoCorBackup.m
+++ AutoCorBackup.m
@@ -1,22 +1,19 @@
-
-
 clear;
 
 format long
 
 % -- Laser Properties --
 c = 3.*10.^8; % Speed of light in m/s
-wavelength = 400.*10.^-9;
+wavelength = 800.*10.^-9;
 k = 2.*pi./wavelength; % Wave vector
-freq = c./wavelength; % Frequency
-omega = 2*pi*freq; % ANGULAR frequency
+freq = 2*pi*(c./wavelength); % Frequency
 T = 1./freq; % Period
-sigma = 107*10^-15; % FWHM of pulse 107*10^-25 for 800nm at 58
+sigma = 80*10^-15; % FWHM of pulse
 
 
 % -- Time Settings --
-numsteps = 12*10^4; % Total steps
-tf = (10)*sigma; %250*10^-15; % Final time (in seconds)
+numsteps = 90*10^4; % Total steps
+tf = 800*10^-15; % Final time (in seconds)
 h = tf./numsteps; % Step size
 tvec = zeros(1, numsteps); % Initialize vector
 Emax = 1; % Arbitrary
@@ -28,7 +25,7 @@ for i=2:numsteps % Step through and set time vector
 end
 
 %-- Tau Settings --
-numstepsTau = 12*10^3; %total number of steps
+numstepsTau = .4*10^3; %total number of steps
 tfTau = tf; %final value of tau
 hTau = tfTau./numstepsTau; %step size of tau
 tauvec = zeros(1,numstepsTau); %initialize vector
@@ -40,41 +37,42 @@ for i=2:numstepsTau % Step through and set tau vector
 end
 
 
+% phase = zeros(1, numsteps); % Initialize phaseshift (position) vector
+% for i=2:numsteps % Set the phase vector off of the times using function
+%     phase(i) = time2pos(tvec(i));
+% end
+
 for i=2:numstepsTau
-    Wave1 = E1(Emax,omega,tvec,twave1,sigma);
-    Wave2 = E2(Emax,omega,tvec,tauvec(i),twave2,sigma);
+    Wave1 = E1(Emax,freq,tvec,twave1,sigma);
+    Wave2 = E2(Emax,freq,tvec,tauvec(i),twave2,sigma);
     E = Wave1+Wave2;
     I = Intensity(E);
     Itot(i)= trapz(tvec,(I.^2)); %numerical integrate function
     %scatter(tvec(1,2:end), I(1,2:end))
-end
 
+    %E = Wave1 ;
+    %scatter(phase(1,2:end), I(1,2:end))
+    %scatter(tv(1,2:end), I(1,2:end))
+    %scatter(tvec(1,2:end), E(1,2:end));
+
+    % function x = time2pos(t) % Simulates the piezo micrometer, varying 1 micron per 1/2 second
+    % %x = 0.0000000000001*sin(2*t);
+    % x = 0.0000000000001*(t);
+    % end
+end
+%scatter(tauvec,Itot,1);
 plot((tauvec*c),Itot)
 
-peaks = [];
-fwhm = max(Itot)./2;
-for i = 3:length(Itot)
-    if Itot(i) > Itot(i-2) && Itot(i-1) > Itot(i)
-        peaks = [peaks;Itot(i-1)];
-    end
-end
-numpeak = 0;
-for i = 1:length(peaks)
-    if peaks(i) > fwhm
-        numpeak = numpeak+1;
-    end
-end
-numpeak
 function I = Intensity(E)
-I = 8*(E).^2;
+I = (8*(E).^2);
 end
 
-function E = E2(Emax,omega,tvec,tau,twave2,sigma)
+function E = E2(Emax,freq,tvec,tau,twave2,sigma)
 numsteps = length(tvec);
 E = zeros(1,numsteps);
 for i=1:numsteps
     %E(i) = Emax*cos(k*(0)-freq*tvec(i)+ phase(i));
-    E(i) = Emax*cos(omega*(tvec(i)-tau-twave2))*exp((-(tvec(i)+tau-twave2).^2)./(2*(sigma^2)));
+    E(i) = Emax*cos(freq*(tvec(i)-tau-twave2))*exp((-(tvec(i)+tau-twave2).^2)./(2*sigma^2));
 end
 end
 
@@ -83,10 +81,6 @@ numsteps = length(tvec);
 E = zeros(1,numsteps);
 for i=1:numsteps
     %E(i) = Emax*cos(k*(0)-freq*tvec(i));
-    E(i) = Emax*cos(freq*(tvec(i)-twave1))*exp(-((tvec(i)-twave1).^2)./(2*(sigma^2)));
+    E(i) = Emax*cos(freq*(tvec(i)-twave1))*exp(-((tvec(i)-twave1).^2)./(2*sigma^2));
 end
 end
-
-
-
-
